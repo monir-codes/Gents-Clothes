3a75042ce6b66c2b88f3afed998f7684bdb93615
unknown <mdrummanmondal2@gmail.com>
2026-08-04 06:46:16 +0600

    fix: Added hardcoded MongoDB Atlas fallback URI for Vercel deployments

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MONGO_URI IS:", process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://gent_fits:bluWk3TM2C2HnFxs@simple-crud-cluster.0hdbxiy.mongodb.net/?appName=Simple-crud-cluster");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
